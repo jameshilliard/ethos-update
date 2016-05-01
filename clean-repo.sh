c2136621e91de6a76c3cd7203221ec30686206f3
Sling00 <sling00@gmail.com>
2016-05-01 17:02:42 -0500

    add cleanup script

---
+++ clean-repo.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+WORKDIR="updates/$1/"
+
+rm -rf $WORKDIR/wip
+rm -rf $WORKDIR/README.md
+rm -rf $WORKDIR/dist
+rm -f $WORKDIR/newupdate.sh
+rm -f $WORKDIR/update.sh
+rm -f $WORKDIR/cleanup.sh
+rm -f $WORKDIR/opt/ethos/sbin/ethos-fan-daemon
+rm -f $WORKDIR/etc/init/ethos-fan-daemon.conf
+rm -f $WORKDIR/home/ethos/.config/autostart/ethos-fan-daemon.desktop
+
