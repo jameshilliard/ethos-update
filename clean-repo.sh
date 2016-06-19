80573c5f6945da7b407cac4efa0e4994fded98a8
Sling00 <sling00@gmail.com>
2016-06-18 23:55:05 -0500

    remove the partial clonescript..

--- clean-repo.sh
+++ clean-repo.sh
@@ -7,6 +7,7 @@ rm -rf $WORKDIR/dist
 rm -f $WORKDIR/newupdate.sh
 rm -f $WORKDIR/update.sh
 rm -f $WORKDIR/cleanup.sh
+rm -f $WORKDIR/clonescript.sh
 rm -f $WORKDIR/opt/ethos/sbin/ethos-fan-daemon
 rm -f $WORKDIR/etc/init/ethos-fan-daemon.conf
 rm -f $WORKDIR/home/ethos/.config/autostart/ethos-fan-daemon.desktop
